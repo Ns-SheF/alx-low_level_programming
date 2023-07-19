86b994e8647bca910494f18d036772228de7d036
SheF <ishehu2016@gmail.com>
2023-07-19 00:16:16 +0000

    AHH! Functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,25 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- *  main - prints the string value putchar
+ *   main -  prints _putchar, followed by a new line
  *
- *  Return: Always 0 (success)
+ *   Return: Always 0
  */
+
 
 int main(void)
 {
-	char *str = '_putchar\n';
 
-	while (*str)
-	{
-		write(1, str, 1);
-		str++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,4 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-int _putchar(char c);
+r(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +10,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
+void print_times_table(int n);
